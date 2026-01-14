ac1ef5fd23adbbad971fc8e9edb1460df68a105a
PIYUSH LAXMAN BHONDAVE <piyushbhondave789@gmail.com>
2026-01-14 19:42:03 +0530

    Initial Setup

---
+++ src/main/java/com/lld/practice/Main.java
@@ -0,0 +1,8 @@
+package com.lld.practice;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, LLD!");
+        System.out.println("Running on Java version: " + System.getProperty("java.version"));
+    }
+}
